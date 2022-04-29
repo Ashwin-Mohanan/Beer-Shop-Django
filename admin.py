d2f28fa2ffd1bcf7c78a3cafa24ab440fde85529
Ashwin-Mohanan <104643570+Ashwin-Mohanan@users.noreply.github.com>
2022-04-30 00:16:06 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from testapp.models import Beer
-# Register your models here.
-class BeerAdmin(admin.ModelAdmin):
-    list_display=('Bname','Taste','color','price')
-admin.site.register(Beer,BeerAdmin)
